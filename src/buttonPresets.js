62ca0586334a322611b4f2315c04962f3c9f5322
Ali Fazelzadeh <afzcode@gmail.com>
2023-09-21 10:37:12 +0330

    Fixed today action when using another calendar rather than Gregorian

--- src/buttonPresets.js
+++ src/buttonPresets.js
@@ -2,7 +2,7 @@ import DateCalendar from "./calendar";
 export default {
     today: {
         content: dp => dp.locale.today,
-        onClick: dp => dp.setViewDate(new DateCalendar().Date()),
+        onClick: dp => dp.setViewDate(new DateCalendar(dp.opts.calendar).Date()),
     },
     clear: {
         content: dp => dp.locale.clear,
